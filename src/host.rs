dc3462801f03adaad067f979dce7376b6923e289
Cinea Zhan <cinea@cinea.com.cn>
2023-12-22 03:00:58 +0800

    完成Rust侧代码

--- src/host.rs
+++ src/host.rs
@@ -1,20 +1,35 @@
 use std::fs;
+use std::fs::File;
 use dirs;
 use flate2::read::GzDecoder;
 use tar::Archive;
+use anyhow::Result;
+use wakatime_annual_report::report::NormalReport;
 
 /// 我们的网页
 const WEB_DIST_TAR: &[u8] = include_bytes!("../web_dist.tar");
 
-pub fn host_init(){
-    let mut temp_dir = dirs::data_dir().unwrap();
-    temp_dir.push("cinea/wakatime-annual-report");
-    fs::create_dir_all(temp_dir.as_path()).unwrap();
+pub fn host_init() {
+    let mut data_dir = dirs::data_dir().unwrap();
+    data_dir.push("cinea/wakatime-annual-report");
+    fs::create_dir_all(data_dir.as_path()).unwrap();
 
     // 解压 & 写入打包完成的前端
     let tar = GzDecoder::new(WEB_DIST_TAR);
     let mut a = Archive::new(tar);
-    a.unpack(temp_dir.as_path()).unwrap();
+    a.unpack(data_dir.as_path()).unwrap();
 }
 
-pub fn host(host:)
+pub fn host(host: &str, port: u16, report: NormalReport) -> Result<()> {
+    let mut dist_dir = dirs::data_dir().unwrap();
+    dist_dir.push("cinea/wakatime-annual-report/dist");
+
+    let mut json_path = dist_dir.clone();
+    json_path.push("output.json");
+    serde_json::to_writer(File::create(json_path.as_path())?, &report)?;
+
+    let server = file_serve::ServerBuilder::new(dist_dir).hostname(host).port(port).build();
+    server.serve()?;
+
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,101 @@
 mod host;
 
 use std::fs::File;
-use std::io::Write;
+use std::io::{stdin, stdout, Write};
+use std::str::FromStr;
 use anyhow::Result;
 use clap::Parser;
+use colored::Colorize;
 
 use wakatime_annual_report::{get_analyses_report_data, read_json_from_file};
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
-    /// Input file name
-    #[arg(short, long)]
+    /// Input file path
+    #[arg(short, long, required = true)]
     input: String,
+
+    /// Don't serve by the program
+    #[arg(long = "no-serve")]
+    no_serve: bool,
+
+    /// The year to generate the report
+    #[arg(long)]
+    year: Option<i32>,
+
+    /// Your time zone, default as 8 (UTC+8).
+    #[arg(long = "time-zone", short, default_value = "8.0")]
+    time_zone: f64,
+
+    /// Timeout of the WakaTime's time calculating. See WakaTime's FAQ for more info.
+    #[arg(long = "time-zone", short, default_value = "900.0")]
+    time_out: f64,
+
+    /// The host address of the web server
+    #[arg(long, default_value = "127.0.0.1")]
+    host: String,
+
+    /// The host port of the web server
+    #[arg(short, long, default_value = "8031")]
+    port: u16,
 }
 
 fn main() -> Result<()> {
+    let args = Args::parse();
+
     pretty_env_logger::init();
 
-    let file_path = r#"C:\Users\cinea\Downloads\wakatime-cinea123163.com-ae526249eb5349709adfaa168f50c84f(1).json"#;
+    if !args.no_serve {
+        host::host_init();
+    }
 
-    println!("Loading data from json...");
-    let data = read_json_from_file(file_path)?;
+    println!("{}", "✨Please wait... We are reading the file...".yellow());
+    let data = read_json_from_file(&args.input)?;
 
-    println!("Analysing data...");
-    let data = get_analyses_report_data(data, 2023, 8.0, 900.0);
+    let mut year = 0;
+    if args.year.is_some() {
+        year = args.year.unwrap();
+    } else {
+        let years = data.get_available_years();
+        if years.len() == 1 {
+            year = years[0];
+        } else {
+            println!("{} {}", "🗓We have found different years in your record:".blue(), years.iter().map(|y| y.to_string()).collect::<Vec<_>>().join(", "));
+            loop {
+                print!("  Please choose one: ");
+                stdout().flush()?;
+                let mut line = String::new();
+                if let Ok(n) = stdin().read_line(&mut line) {
+                    if n > 0 {
+                        if let Ok(y) = i32::from_str(line.trim()) {
+                            if years.contains(&y) {
+                                year = y;
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
 
-    serde_json::to_writer(File::create("./output.json")?, &data)?;
+    println!("{}", format!("🔍Analysing your {}...", year).green());
 
-    println!("Finished...");
+    println!("  {} If you are not in China, remember to configure the time zone in the arguments!", "Tips".green());
+
+    let data = get_analyses_report_data(data, year, args.time_zone, args.time_out);
+
+    if args.no_serve {
+        serde_json::to_writer(File::create("./output.json")?, &data)?;
+        println!("{} {} {}", "😄The".green(), "output.json".blue(), "is successfully generated.".green())
+    } else {
+        println!("{}", "😄The analysis is successful and we are starting the server for you...".green());
+        println!("{} {}", "🌍Open in your browser: ".green(), format!("http://localhost:{}", args.port).blue());
+        println!("{}", "  And click Ctrl+C to exit.".green());
+
+        host::host(&args.host, args.port, data)?;
+    }
 
     Ok(())
 }
