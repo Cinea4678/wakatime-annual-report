0f245170f94e662b9b7d9c5b44b62b08b4a0ac82
Cinea Zhan <cinea@cinea.com.cn>
2023-12-22 03:10:54 +0800

    修改前端

--- webpage/src/views/report/ReportView.vue
+++ webpage/src/views/report/ReportView.vue
@@ -49,14 +49,17 @@ onMounted(() => {
 </script>
 
 <template>
-  <div class="relative top-0 bottom-0 left-0 right-0 px-[3vw]">
-    <div class="absolute top-[7vh]">
+  <div class="relative top-0 min-h-[100vh] left-0 right-0 px-[3vw]">
+    <div class="pt-[7vh]">
       <div class="font-light text-2xl">WakaTime 年度报告</div>
       <div class="mt-3">
         Page {{ page + 1 }} / {{ pages.length }} - {{ pages[page].name }}
       </div>
     </div>
-    <div class="absolute top-[90vh] flex gap-3 items-center">
+    <div class="mt-10">
+      <component :is="pages[page].component" />
+    </div>
+    <div class="absolute bottom-[5vh] flex gap-3 items-center">
       <a-button
         :disabled="page == 0"
         :size="lg ? 'large' : 'middle'"
@@ -72,9 +75,6 @@ onMounted(() => {
         下一页
       </a-button>
       <div v-else class="font-light">已是最后一页</div>
-    </div>
-    <div class="relative top-[20vh]">
-      <component :is="pages[page].component" />
     </div>
   </div>
 </template>
