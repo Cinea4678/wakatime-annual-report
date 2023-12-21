f3e02a969c4594aa93ce5d7b1dfa372f478a481f
Zhang Yao <cineazhan@gmail.com>
2023-12-22 01:08:38 +0800

    携带dist

--- webpage/src/utils/router.ts
+++ webpage/src/utils/router.ts
@@ -1,12 +1,13 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router"
 
 const routes = [
   { path: "/", component: () => import("@/views/HomeView.vue") },
   { path: "/error", component: () => import("@/views/ErrorView.vue") },
   { path: "/report", component: () => import("@/views/report/ReportView.vue") },
-];
+]
 
+// 由于我们的服务器不一定支持路由回退，因此就不使用WebHistory了
 export default createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes,
-});
+})
